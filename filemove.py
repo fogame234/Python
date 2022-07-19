d137b82b1edac806a558b3ca436e87bbf83b24f1
Fogame <tredavi1@protonmail.com>
2022-07-19 17:46:10 -0600

    Update filemove.py
    Removed choice input and variables based on what extension the file is. Added the final else to move things to another location if it doesn't meet the specified extensions.

--- filemove.py
+++ filemove.py
@@ -2,38 +2,66 @@ import os
 import shutil
 from pathlib import Path
 from tkinter import filedialog
-from tkinter import *
 
 fileWindow = Tk()
 fileWindow.withdraw()
-source_folder = filedialog.askdirectory()
-dest_folder = ""
-extension = ""
+source_folder = Path(filedialog.askdirectory()) 
+extension = ['.csv', '.xlsx', '.xls', '.sql']
 
-def fileChoice(dest_folder, extension):
-    choice = input("Moving CSV or SQL files? ")
+def get_new_file_name(file_name, duplicate_num):
+  # handle degenerate case of 0
+  if duplicate_num == 0:
+    return file_name
 
-    if choice == "csv":
-        fileWindow = Tk()
-        fileWindow.withdraw()
-        dest_folder = filedialog.askdirectory()        
-        extension = '.csv'
+  # Split the path into the file stem and the extension
+  # Docs for os.path.splitext: https://docs.python.org/3/library/os.path.html#os.path.splitext
+  stem, ext = os.path.splitext(file_name)
 
-    if choice == "sql":
-        fileWindow = Tk()
-        fileWindow.withdraw()
-        dest_folder = filedialog.askdirectory()   
-        extension = '.sql'
+  # Concatenate the duplicate num w/ file stem and add ext
+  # For example file.txt with duplicate_num of 3 would become file_3.txt
+  return stem + "_" + str(duplicate_num) + ext
 
-    return dest_folder, extension
-
-dest_folder, extension = fileChoice(dest_folder, extension)
+def move_file(source_folder, dest_folder, file_name, duplicate_num=0):
+  new_file_name = get_new_file_name(file_name, duplicate_num)
+  if os.path.exists(os.path.join(dest_folder, new_file_name)):
+      move_file(source_folder, dest_folder, file_name, duplicate_num + 1)
+  else:
+      src_file = os.path.join(source_folder, file_name)
+      dst_file = os.path.join(dest_folder, new_file_name)
+      shutil.move(src_file, dst_file)
 
 for root, dirs, files in os.walk(source_folder):
+
     for fn in files:
         src_path = Path(root).joinpath(fn)
-        dest_path = dest_folder.joinpath(src_path.name)
-        if src_path.suffix == extension:
+        rel_path = os.path.relpath(src_path, source_folder)
+        if src_path.suffix in extension:
+            if '_' in root:
+                continue
+            else:
+                if src_path.suffix == ".sql":
+                    dest_folder = r"C:\Users\Trevor\Documents\TestVericloud\sql"
+                    dest_path = Path(dest_folder).joinpath(rel_path)                    
+                if src_path.suffix != ".sql":
+                    dest_folder = r"C:\Users\Trevor\Documents\TestVericloud\CSV"
+                    dest_path = Path(dest_folder).joinpath(rel_path) 
+                print("Scanning: " + str(src_path))
+                if os.path.isfile(src_path):
+                    parentDir = Path(src_path).parent
+                    isfile = True
+                    if isfile and parentDir == source_folder:
+                        move_file(root, dest_folder, fn)
+                        print("File Moved: " + fn)
+                        continue
+                dest_path.parent.mkdir(parents=True, exist_ok=True)
+                move_file(src_path.parent, dest_path.parent, fn)
+                print("File Moved: " + str(src_path))
+                if len(os.listdir(src_path.parent)) == 0:
+                    os.rmdir(src_path.parent)
+                    print("Removed: " + str(src_path.parent))
+        else:
+            noPassword = (source_folder).joinpath("_NO_PASSWORDS")
+            dest_no = (noPassword.joinpath(rel_path))
             if '_' in root:
                 continue
             else:
@@ -41,15 +69,10 @@ for root, dirs, files in os.walk(source_folder):
                     parentDir = Path(src_path).parent
                     isfile = True
                     if isfile and parentDir == source_folder:
-                        shutil.move(str(src_path), str(dest_folder))
-                        print("File Moved: " + fn)
                         continue
-                if not os.path.exists(root):
-                    print("File already moved: " + fn)
-                    continue
-                if not dest_path.exists():
-                    shutil.move(str(root), str(dest_folder))
-                    print("File Moved: " + fn)
-
-
-
+                dest_no.parent.mkdir(parents=True, exist_ok=True)
+                move_file(src_path.parent, dest_no.parent, fn)
+                print("File Moved: " + str(src_path))
+                if len(os.listdir(src_path.parent)) == 0:
+                    os.rmdir(src_path.parent)
+                    print("Removed: " + str(src_path.parent))
